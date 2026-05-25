33f503165d575f55b803af7e16db7bb135defed5
Alex Richardson <william.a.richardson@vanderbilt.edu>
2026-05-25 15:14:46 -0500

    Patched I24 Map downloading.

--- I24_CARLA_download.sh
+++ I24_CARLA_download.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd Import
-wget -O FinalMapFinal.tar.gz -L https://vanderbilt.box.com/shared/static/540tucccmdioqspxzqwa18e14zymg3i0.gz
+wget -O FinalMapFinal.tar.gz -L https://vanderbilt.box.com/v/i24carlauncooked
